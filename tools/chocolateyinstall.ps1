595a5bed6426dddd4c3d5af06b547c85f725a2c2
Adam Tahri <gotakk@gmail.com>
2021-03-12 12:32:00 +0100

    Initial commit

---
+++ tools/chocolateyinstall.ps1
@@ -0,0 +1,21 @@
+﻿$ErrorActionPreference = 'Stop'; # stop on all errors
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url64      = 'https://github.com/iotaledger/goshimmer/releases/download/v0.5.0/goshimmer-0.5.0_Windows_x86_64.zip' # 64bit URL here (HTTPS preferred) or remove - if installer contains both (very rare), use $url
+$unzipLocation = "$env:ProgramFiles\IOTA Foundation\GoShimmer\"
+
+$GoShimmerPackageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $unzipLocation
+  fileType      = 'EXE'
+  url64bit      = $url64
+  softwareName  = 'goshimmer*'
+  checksum      = ''
+  checksumType  = 'sha256' #default is md5, can also be sha1, sha256 or sha512
+  checksum64    = '3d5fc807eff52369ea7bdbe51dd931fa616546db6227c1423af894b605c804a5'
+  checksumType64= 'sha256' #default is checksumType
+  validExitCodes= @(0, 3010, 1641)
+}
+
+Install-ChocolateyZipPackage @GoShimmerPackageArgs
+Get-ChocolateyUnzip -FileFullPath "$toolsDir\goshimmer-launcher.zip" -Destination $unzipLocation
+Install-ChocolateyPath "$env:ProgramFiles\IOTA Foundation\GoShimmer\" 'User'
---
+++ tools/chocolateyuninstall.ps1
@@ -0,0 +1,11 @@
+﻿$installLocation = "$env:ProgramFiles\IOTA Foundation\GoShimmer\"
+
+Write-Host "Removing $installLocation folder..."
+Remove-Item $installLocation -Recurse -Force
+
+Write-Host "Removing $installLocation from PATH..."
+$path = [System.Environment]::GetEnvironmentVariable('PATH', 'User')
+$path = ($path.Split(';') | Where-Object { $_ -ne $installLocation }) -join ';'
+[System.Environment]::SetEnvironmentVariable('PATH', $path, 'User')
+
+Write-Host "Done."
